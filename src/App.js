efb9fcd8d9dd89ee4e70a14f000e3fb39122db16
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2023-12-05 03:03:04 +0100

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">Portfolio </header>
     </div>
   );
